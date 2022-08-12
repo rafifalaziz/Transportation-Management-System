b26034b184657415c09764e8028810733b00b406
rafifalaziz <mrafifalaziz26@gmail.com>
2022-08-13 06:19:22 +0700

    add plate, truck type endpoint

--- controller/truck.js
+++ controller/truck.js
@@ -1,6 +1,47 @@
 const models = require("../models");
 
-const {TRUCK} = models.init;
+const {TRUCK, PLATE_TYPE, TRUCK_TYPE} = models.init;
+
+exports.addPlate = async (req, res) => {
+    try {
+        const plate = await PLATE_TYPE.create({...req.body});
+        res.status(200).send({
+            success: true,
+            message: 'Berhasil membuat plate',
+            code: 200,
+            plate,
+        }); 
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: 'Gagal membuat plate',
+            code: 500,
+            error,
+        });
+    }
+}
+
+exports.addTruckType = async (req, res) => {
+    try {
+        const truckType = await TRUCK_TYPE.create({...req.body});
+        res.status(200).send({
+            success: true,
+            message: 'Berhasil membuat truck type',
+            code: 200,
+            truckType,
+        }); 
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: 'Gagal membuat truck type',
+            code: 500,
+            error,
+        });
+    }
+}
+
 
 exports.addTruck = async (req, res) => {
     try {
@@ -9,7 +50,7 @@ exports.addTruck = async (req, res) => {
             success: true,
             message: 'Berhasil membuat truck',
             code: 200,
-            user,
+            truck,
         }); 
     } catch (error) {
         console.log(error);
--- routes/truck.js
+++ routes/truck.js
@@ -4,5 +4,7 @@ const express = require("express");
 const router = express.Router();
 
 router.post('/add', truck.addTruck);
+router.post('/addPlate', truck.addPlate);
+router.post('/addTruckType', truck.addTruckType);
 
 module.exports = router;
