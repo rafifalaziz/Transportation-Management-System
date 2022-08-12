73342fdbc6b829ea2a9b3110bac92c7274e27482
rafifalaziz <mrafifalaziz26@gmail.com>
2022-08-13 04:06:53 +0700

    boiler plate

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import express from "express";
+import routes from "./routes/index.js";
 const app = express();
 
-//app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-//app.use('/', routes);
+app.use('/', routes);
 app.all('*', (req, res) => res.status(404).send({
   success: false,
   message: 'route tidak ditemukan',
