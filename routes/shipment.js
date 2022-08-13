d9e5ed267fe6f20ec96be7fda22e97929db41be4
rafifalaziz <mrafifalaziz26@gmail.com>
2022-08-13 09:47:35 +0700

    get all shipment

--- routes/shipment.js
+++ routes/shipment.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const {addShipment, updateShipment} = require("../controller/shipment");
+const {addShipment, updateShipment, getAllShipment} = require("../controller/shipment");
 
 const router = express.Router();
 
 router.post('/', addShipment)
 router.put('/update/:id', updateShipment)
+router.get('/all', getAllShipment)
 
 module.exports = router;
