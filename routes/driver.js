0cd155cd8a1f9eade6ff34f5621b48588418428f
rafifalaziz <mrafifalaziz26@gmail.com>
2022-08-13 09:13:29 +0700

    minor fix

--- routes/driver.js
+++ routes/driver.js
@@ -2,6 +2,8 @@ const express = require("express");
 const {addDriver, getDrivers, editDriver} = require("../controller/driver")
 const {auth} = require("../middleware");
 
+const router = express.Router();
+
 router.post('/add', auth.transporterRole, addDriver);
 router.put('/edit/:id', auth.transporterRole, editDriver);
 router.get('/', auth.transporterRole, getDrivers)
