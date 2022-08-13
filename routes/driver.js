9e7a28046306d3c487ff5aaa34e4a585ee9d34cb
MuhammadAlfariziT <muhammad.tazkia@tiket.com>
2022-08-13 16:19:47 +0700

    feat: add simple route

--- routes/driver.js
+++ routes/driver.js
@@ -1,4 +1,6 @@
 const express = require("express");
+
+const {addDriver, getDrivers, editDriver} = require("../controller/driver")
 const {auth} = require("../middleware");
 
 const router = express.Router();
@@ -6,7 +8,7 @@ const {addDriver, getDrivers, editDriver, searchDriverByName} = require("../cont
 
 
 router.get('/search', auth.transporterRole, searchDriverByName)
-
+router.get('/search', auth.transporterRole, searchDriverByName)
 router.post('/add', auth.transporterRole, addDriver);
 router.put('/edit/:id', auth.transporterRole, editDriver);
 router.get('/', auth.transporterRole, getDrivers)
